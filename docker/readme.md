59b02777bedb6974ce9dbf4c6cc8c2058a25d93d
anyongjin <anyongjin163@163.com>
2023-10-05 19:46:30 +0800

    update Dockerfile

--- docker/readme.md
+++ docker/readme.md
@@ -1,12 +1,5 @@
-因最开始在美国VPS部署无法直接访问币安api，所以加了国内代理。这是支持打包代理的镜像版本。
-
-```shell
-# 在此目录编译镜像
-docker build . -t python39
-
-# 启动容器
-docker run -d -v /root:/root --name highfreq --network=host --cap-add=NET_ADMIN python39
-
-# 进入容器
-docker exec -it highfreq /bin/bash
+# 鍩虹闀滃儚缂栬瘧
+```dockerfile
+FROM python:3.11.6
+RUN pip install --upgrade --force-reinstall -r requirements.txt
 ```
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # File  : setup.py
 # Author: anyongjin
 # Date  : 2023/1/28
-from setuptools import setup, find_packages
+from setuptools import setup
 '''
 暂不需要
 binance-connector需要3.0.0rc1版本，请勿通过pip install安装。可Git clone然后python setup.py install 安装
@@ -17,7 +17,6 @@ setup(
     author='anyongjin',
     author_email='anyongjin163@163.com',
     description='high freq trade',
-    packages=find_packages(),
     install_requires=requirements,
     extras_require={
         "develop": [
